c05b57e325bf91f45bf87175134a4d12596219e5
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2021-07-28 07:23:05 +0530

    Renting benefit changed

--- index.js
+++ index.js
@@ -192,7 +192,7 @@ function hrc(a) {
      and mothly expenditure of `+ (efemi - er) + ` will be saved`;
     o.innerHTML += `<br>If we assume the rate of growth to be ` + roi + `% then you will save `
         + h + ` as downpayement amount and ` +
-        i + ` as emi amount ` + `so your net gain will be ` + (h + i);
+        i + ` as emi amount ` + `so your net gain will be ` + (h + i - localStorage["tr"] + er * 12 * y);
     localStorage["rg"] = h + i;
     rg = localStorage["rg"];
     lrc(1);
